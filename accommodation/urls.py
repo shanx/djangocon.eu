153d7bc3036c362b58eb3ca8994fb5f4bc572010
Remco Wendt <remco@maykinmedia.nl>
2011-03-14 00:45:27 +0100

    Use string for linking view to url

--- accommodation/urls.py
+++ accommodation/urls.py
@@ -2,11 +2,9 @@ from __future__ import absolute_import
 
 from django.conf.urls.defaults import *
 
-from .views import reserve, reservation_received
-
 urlpatterns = patterns('accommodation.views',
-    url(r'^reserve/$', reserve, name='reserve'),
-    url(r'^reservation_received/$', reservation_received, name='reservation_received'),
+    url(r'^reserve/$', 'reserve', name='reserve'),
+    url(r'^reservation_received/$', 'reservation_received', name='reservation_received'),
 )
 
 
