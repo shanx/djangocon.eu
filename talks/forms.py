d953744a6ef51990f8acc1713dec3d4d4dd51b81
Remco Wendt <remco@maykinmedia.nl>
2011-03-13 22:57:45 +0100

    Text updates

--- talks/forms.py
+++ talks/forms.py
@@ -6,7 +6,7 @@ from django.utils.translation import ugettext_lazy as _
 from .models import Talk
 
 class TalkForm(forms.ModelForm):
-    allow_recording = forms.BooleanField(label=_('i permit the recording and streaming of my talk'), required=True)
+    allow_recording = forms.BooleanField(label=_('I permit the recording and streaming of my talk'), required=True)
 
     level = forms.ChoiceField(label=_('audience level'), widget=forms.RadioSelect(), choices=Talk.LEVEL_CHOICES, required=True)
     length = forms.ChoiceField(label=_('talk length'), widget=forms.RadioSelect(), choices=Talk.LENGTH_CHOICES, required=True)
