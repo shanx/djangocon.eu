127a4055e1fd1242e8a20eb163acaf7c95c8146d
Remco Wendt <remco@maykinmedia.nl>
2011-03-14 00:13:06 +0100

    Added thank you page and renamed template

--- accommodation/urls.py
+++ accommodation/urls.py
@@ -7,6 +7,7 @@ from .views import reserve
 
 urlpatterns = patterns('djangocon.accommodation.views',
     url(r'^reserve/$', reserve, name='reserve'),
+    url(r'^reservation_received/$', reserve, name='reservation_received'),
 )
 
 
--- accommodation/views.py
+++ accommodation/views.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.core.urlresolvers import reverse
 
 from .models import Hotel
 from .forms import ReservationForm, CreditcardForm
@@ -17,14 +18,12 @@ def reserve(request):
             reservation = reservation_form.save()
 
             # TODO: send email
-            # TODO: redirect to thank you page
-
-            return HttpResponseRedirect()
+            return HttpResponseRedirect(reverse('accommodation:reservation_received'))
     else:
         reservation_form = ReservationForm()
         creditcard_form = CreditcardForm()
 
-    return render_to_response("accommodation/reservation.html", {
+    return render_to_response("accommodation/reserve.html", {
         'reservation_form': reservation_form,
         'creditcard_form': creditcard_form,
         'hotels': hotels,
