2fd45f1ec3f64bac72d42872b690f388dfbf7060
Remco Wendt <remco@maykinmedia.nl>
2011-03-14 23:45:18 +0100

    Removed creditcard stuff and added confirmation mail sending

--- accommodation/forms.py
+++ accommodation/forms.py
@@ -9,17 +9,3 @@ from .models import Reservation
 class ReservationForm(forms.ModelForm):
     class Meta:
         model = Reservation
-
-# Don't store any creditcard information locally
-class CreditcardForm(forms.Form):
-    CREDITCARDCOMPANY_CHOICES = (
-        ('visa', 'Visa'),
-        ('mastercard', 'MasterCard'),
-        ('americanexpress', 'American Express'),
-        ('dinersclub', 'Diners Club'),
-    )
-
-    company = forms.ChoiceField(label=_('Creditcard Company'), required=True, choices=CREDITCARDCOMPANY_CHOICES)
-    card_number = forms.CharField(label=_('Creditcard number'), required=True)
-    expiry_date = forms.DateField(label=_('Expiry date'), required=True)
-    cvc_code = forms.CharField(label=_('CVC Code'), required=True)
--- accommodation/views.py
+++ accommodation/views.py
@@ -4,29 +4,45 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.core.urlresolvers import reverse
 from django.views.generic.simple import direct_to_template
+from django.template.loader import render_to_string
+from django.core.mail import send_mail
+from django.conf import settings
+from django.http import HttpResponseRedirect
+
 
 from .models import Hotel
-from .forms import ReservationForm, CreditcardForm
+from .forms import ReservationForm
 
 def reserve(request):
     hotels = Hotel.objects.all()
 
     if request.method == 'POST':
         reservation_form = ReservationForm(request.POST)
-        creditcard_form = CreditcardForm(request.POST)
 
-        if reservation_form.is_valid() and creditcard_form.is_valid():
+        if reservation_form.is_valid():
             reservation = reservation_form.save()
 
-            # TODO: send email
+            subject = '[DjangoCon]: Accommodation reservation received'
+            msg = render_to_string('accommodation/confirmation_email.txt',
+                { 'reservation': reservation })
+            from_address = 'no-reply@djangocon.eu'
+
+            # Send confirmation e-mail to Djangonaut
+            send_mail(subject, msg, from_address,
+                [reservation.email_address],
+                fail_silently=False)
+
+            # Send confirmation e-mail to supplier
+            send_mail(subject, msg, from_address,
+                [settings.ACCOMMODATION_RESERVATION_EMAIL],
+                fail_silently=False)
+
             return HttpResponseRedirect(reverse('accommodation:reservation_received'))
     else:
         reservation_form = ReservationForm()
-        creditcard_form = CreditcardForm()
 
     return render_to_response("accommodation/reserve.html", {
         'reservation_form': reservation_form,
-        'creditcard_form': creditcard_form,
         'hotels': hotels,
     }, context_instance=RequestContext(request))
 
