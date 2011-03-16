a760b78775883eb398bf15e6f412f62ad59e17d9
Remco Wendt <remco@maykinmedia.nl>
2011-03-16 11:13:03 +0100

    Removed attendees app, this is done through an external ticketing agency

--- utils/views.py
+++ utils/views.py
@@ -10,7 +10,6 @@ from django.contrib.admin.views.decorators import staff_member_required
 from subscribers.models import Subscriber
 from subscribers.forms import SubscriberForm
 from blog.models import Post
-from attendees.models import TicketType
 
 try:
     subscription_cookie = getattr(settings, 'SUBSCRIPTION_COOKIE_NAME')
@@ -42,39 +41,3 @@ def server_error(request, template_name='500.html'):
         'MEDIA_URL': settings.MEDIA_URL,
         'STATIC_URL': settings.STATIC_URL,
     })))
-
-def home(request):
-    """
-    Renders the homepage.
-    
-    Templates: `home.html`
-    Context:
-        
-    """
-    
-    set_cookie = False
-    if request.method == 'POST':
-        sf = SubscriberForm(request.POST)
-        if sf.is_valid():
-            s, created = Subscriber.objects.get_or_create(
-                email=sf.cleaned_data['email'],
-                defaults={'subscribed_from':request.META['REMOTE_ADDR'],})
-                        
-            set_cookie = True
-            sf = SubscriberForm()
-    else:
-        sf = SubscriberForm()
-    
-    context = {}
-    context['blogpost'] = Post.objects.published().latest()
-    context['tickettypes'] = TicketType.objects.filter(is_visible=True)
-    context['form'] = sf
-    context['subscribed'] = set_cookie or subscription_cookie in request.COOKIES
-    
-    response = render_to_response(
-        'home.html',
-        context,
-        context_instance=RequestContext(request))
-    if set_cookie:
-        response.set_cookie(subscription_cookie, max_age=31556926)
-    return response
