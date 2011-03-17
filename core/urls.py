ca5b24cd174ce8fda29f7836bb263946deb29612
Idan Gazit <idan@gazit.me>
2011-03-17 12:24:43 +0200

    Added IE blocker.

--- core/urls.py
+++ core/urls.py
@@ -4,15 +4,13 @@ from django.views.decorators.cache import cache_page
 
 urlpatterns = patterns('core.views',
     url(r'^$', 'home', name='home'),
+    
+    # Static pages that have no interaction
     url(r'^tickets/$', 'cached_direct', {'template': 'core/tickets.html'}, name='tickets'),
     url(r'^sponsors/$', 'cached_direct', {'template': 'core/sponsors.html'}, name='sponsors'),
     url(r'^admin/$', 'cached_direct', {'template': 'core/admin.html'}, name='fakeadmin'),
+    url(r'^about/$', 'cached_direct', {'template': 'core/about.html'}, name='about'),
+    url(r'^venues/$', 'cached_direct', {'template': 'core/venues.html'}, name='venues'),
+    url(r'^EINSUFFICIENTBROWSER/$', 'cached_direct', {'template': 'core/ie.html'}, name='ieblocker'),
 )
 
-# Static pages that have no interaction go here
-urlpatterns += patterns('',
-    url(r'^tickets/$', cache_page(direct_to_template), {'template': 'core/tickets.html'}, name='tickets'),
-    url(r'^sponsors/$', cache_page(direct_to_template), {'template': 'core/sponsors.html'}, name='sponsors'),
-    url(r'^about/$', cache_page(direct_to_template), {'template': 'core/about.html'}, name='about'),
-    url(r'^venues/$', cache_page(direct_to_template), {'template': 'core/venues.html'}, name='venues'),
-)
