7af5f9dec9fbd79d9d641d26bbcd2c2932fc685c
Idan Gazit <idan@gazit.me>
2011-04-12 12:46:11 +0300

    Added fields to talks admin.

--- talks/admin.py
+++ talks/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from talks.models import Talk
 
 class TalkAdmin(admin.ModelAdmin):
-    list_display = ('title', 'speakers_list', 'accepted', 'scheduled',)
-    list_filter = ('accepted', 'scheduled',)
+    list_display = ('title', 'speakers_list', 'level', 'length', 'accepted', 'scheduled',)
+    list_filter = ('level', 'length', 'accepted', 'scheduled',)
     search_fields = ('title', 'abstract',)
 
 admin.site.register(Talk, TalkAdmin)
