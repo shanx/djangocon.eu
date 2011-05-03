d0c0362356320c5ffc005aca6c698da12a5442b8
Remco Wendt <remco@maykinmedia.nl>
2011-05-03 15:27:33 +0200

    Gave lightning talks its own top level menu item

--- talks/urls.py
+++ talks/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('talks.views',
     url(r'^(\d+)/$', 'talk_detail', name='talk_detail'),
-    url(r'^submit/$', 'lightningtalk_submit', name='submit'),
+    url(r'^submit/$', 'submit', name='submit'),
+    url(r'^lightning/submit/$', 'lightningtalk_submit', name='lightningtalk_submit'),
     url(r'^thankyou/$', 'thankyou', name='thankyou'),
 )
