9f0eb0751ab89842193ceb9cf4a5231d77d67b1e
Remco Wendt <remco@maykinmedia.nl>
2011-05-11 15:56:41 +0200

    Made absolute URL relative

--- waitlist/urls.py
+++ waitlist/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import *
 from django.views.generic.simple import redirect_to
 
 urlpatterns = patterns('waitlist.views',
-    url(r'^$', redirect_to, {'url': '/waitlist/submit/'}),
+    url(r'^$', redirect_to, {'url': 'submit/'}),
     url(r'^submit/$', 'submit', name='submit'),
     url(r'^thankyou/$', 'thankyou', name='thankyou'),
 )
