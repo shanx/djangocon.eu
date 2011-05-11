0915518264eabf546ca62eb0e54418dd3359f759
Remco Wendt <remco@maykinmedia.nl>
2011-05-11 17:50:02 +0200

    Easily sync remote data to local db

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,7 @@
+import tempfile
 from fabric.api import local, env
+
+APPS_TO_SYNC = ('accommodation', 'blog', 'schedule', 'speakers', 'talks', 'waitlist')
 
 def production():
     env['epioapp'] = 'djangocon-eu-2011'
@@ -12,6 +15,13 @@ def deploy():
     epio('django syncdb')
     epio('django migrate')
     epio('django flush_cache')
+
+def sync_data():
+    with tempfile.NamedTemporaryFile(suffix='.json') as dumpfile:
+        epio('django dumpdata {0} > {1}'.format(
+            ' '.join(APPS_TO_SYNC),
+            dumpfile.name))
+        local('./manage.py loaddata {0}'.format(dumpfile.name))
 
 def epio(commandstring):
     local("epio {0} -a {1}".format(
