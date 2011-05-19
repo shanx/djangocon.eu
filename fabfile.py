a36c91748c80a835ac7bff75cbd5486646b5ea55
Remco Wendt <remco@maykinmedia.nl>
2011-05-19 12:26:08 +0200

    Added sponsors to data sync

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,7 @@
 import tempfile
 from fabric.api import local, env
 
-APPS_TO_SYNC = ('accommodation', 'blog', 'schedule', 'speakers', 'talks', 'waitlist')
+APPS_TO_SYNC = ('accommodation', 'blog', 'schedule', 'speakers', 'talks', 'waitlist', 'sponsors')
 
 def production():
     env['epioapp'] = 'djangocon-eu-2011'
