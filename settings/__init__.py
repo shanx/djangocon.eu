635e2dc5ff6dd9fe2a3d0274664eb91b07fe2f55
Remco Wendt <remco.wendt@gmail.com>
2013-06-17 21:29:28 +0200

    removed ep.io

--- settings/__init__.py
+++ settings/__init__.py
@@ -1,11 +1,8 @@
 from __future__ import absolute_import
 import os
 
-if 'EPIO' in os.environ:
-    from .epio import *
-else:
-    try:
-        from .local import *
-    except ImportError:
-        from .base import *
+try:
+    from .local import *
+except ImportError:
+    from .base import *
 
